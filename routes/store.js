5ce13041014a4b04a2383485dce5d7549120c234
Sissel Buholm Børøsund <sisselb@gmail.com>
2015-09-21 14:33:32 +0200

    adding use of env. var

--- routes/store.js
+++ routes/store.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('store', { title: 'Express Store' });
+  res.render('store', { title: 'Express Store', message_url: process.env.MESSAGE_URL });
 });
 
+router.route('/message').post(function (req, res, next) {
+  console.log('Receiving message ' + req.body.message);
+  res.send('Message was ' + req.body.message);   
+});
+        
 module.exports = router;
