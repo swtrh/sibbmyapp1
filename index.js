dbefe151a9fe574bfc3a938a2c139826af13d3c1
Sissel Buholm Børøsund <sisselb@gmail.com>
2015-09-21 11:39:51 +0200

    update port

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+var port = (process.env.PORT || '3000');
+
+
 app.get('/', function (req,res){
 	res.send('Hello World')
 });
 
+app.listen(port);
 console.log("App started, listening at port 3000")
-
-app.listen(3000);
